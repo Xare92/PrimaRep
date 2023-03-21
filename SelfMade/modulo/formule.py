6d46001f377cebef337b1d3baf9c0a4708b6c559
Xare92 <126160220+Xare92@users.noreply.github.com>
2023-03-21 14:12:02 +0100

    Add files via upload

---
+++ SelfMade/modulo/formule.py
@@ -0,0 +1,15 @@
+def formula_rettangolo(a:float,b:float):
+    '''questa funzione calcola l'area e il perimetro del rettangolo/quadrato'''
+    area = a*b 
+    perimetro = 2*(a+b)
+    return area,perimetro
+
+def somma_numeri(a,b,c):
+    print('questa funzione somma due numeri')
+    risultato = a+b+c
+    type(a)
+    type(b)
+    print(type(c))
+    print('il risultato è '+str(risultato)) 
+
+    
